2b1d55573b79ba9d99584eafa7b30bb525b79631
Matt Bannister <matt.bann@live.co.uk>
2024-08-05 01:22:53 +0100

    Inital commit

---
+++ client.cpp
@@ -0,0 +1,172 @@
+// Networking headers
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <Windows.h>
+
+// Threading headers
+#include <thread>
+#include <mutex>
+
+// other 
+#include <iostream>
+
+// #pragma comment(lib,"Ws2_32.lib")
+
+#define GETSOCKETERRNO() (WSAGetLastError())
+#define ISVALIDSOCKET(s) ((s) >= 0)
+
+SOCKET socket_peer;
+struct addrinfo* peer_address;
+
+bool create(std::string hostName, std::string port)
+{
+	// Not filled in 
+	if (hostName.c_str() == NULL || port.c_str() == NULL){
+		return false;
+	}
+
+	// Set windows socket version
+#ifdef _WIN32
+	WSADATA d;
+	if (WSAStartup(MAKEWORD(2, 2), &d)) {
+		std::cerr << "Failed to initalize." << std::endl;
+		return false;
+	}
+#endif
+
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	// TCP UDP(SOCK_DGRAM)
+	hints.ai_socktype = SOCK_STREAM;
+	// struct addrinfo* peer_address;
+	if (getaddrinfo(hostName.c_str(), port.c_str(), &hints, &peer_address)) {
+		std::cerr << "mTCP::ClientCreateTCP error.\ngetaddrinfo() failed. (" << GETSOCKETERRNO() << ")" << std::endl;
+		return false;
+	}
+
+	printf("Remote address is: "); // TODO remove
+	// Again variable arrays potentially
+	char address_buffer[100];
+	char service_buffer[100];
+	getnameinfo(peer_address->ai_addr, peer_address->ai_addrlen,
+		address_buffer, sizeof(address_buffer),
+		service_buffer, sizeof(service_buffer),
+		NI_NUMERICHOST);
+	printf("%s %s\n", address_buffer, service_buffer); // TODO remove
+
+	// Create socket
+	printf("Creating socket...\n");
+	// SOCKET socket_peer;
+	socket_peer = socket(peer_address->ai_family,
+		peer_address->ai_socktype, peer_address->ai_protocol);
+	if (!ISVALIDSOCKET(socket_peer)) {
+		std::cerr << "mTCP::ClientCreateTCP error.\nsocket() failed. (" << GETSOCKETERRNO() << ")" << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool connect()
+{
+	// Connect
+	printf("Connecting...\n");
+	
+	if (connect(socket_peer,
+		peer_address->ai_addr, peer_address->ai_addrlen)) {
+		
+		fprintf(stderr, "connect() failed. (%d)\n", GETSOCKETERRNO());
+		return false;
+	}
+	freeaddrinfo(peer_address);
+
+	printf("Connected.\n");
+    
+    return true;
+}
+
+std::string recv()
+{	
+
+    // Idea so far: (for server)
+    // Main thread spools up worker thread.
+    // Main thread then deals with displaying to screen and taking user input for sending.
+    // 
+    // Worker thread cycles through all handled connections. Any connections taht have waiting data
+    // will be dealt with and their message will be put into a buffer which main thread will periodically
+    // check to update the screen.
+    //
+    // Worker therad will also check a flag to see if there are any incoming connections. If so 
+    // an async task will be spooled up to accept the connection and add it to connection list.
+    //
+    //
+
+
+    // Using poll as this is used on both windwos and linux
+    
+    // typedef struct pollfd {
+    //   SOCKET fd;
+    //   SHORT  events;
+    //   SHORT  revents; // Used as a return from WSAPoll, can tell result of oporation
+    // } WSAPOLLFD, *PWSAPOLLFD, *LPWSAPOLLFD;
+    WSAPOLLFD fds[1];
+
+    fds[0].fd = socket_peer;
+    fds[0].events = POLLRDNORM;
+
+    // Sockets, number of sockets, timeout (ms)
+    int retCode = WSAPoll(fds, 1, 1);
+    if (retCode == SOCKET_ERROR)
+    {
+        printf("Error occured, errno: %d", errno);
+        return "";
+    }
+    else if (retCode == 0)
+    {
+        printf("Nothing yet.");
+        return "";
+    }
+    else
+    {
+        printf("Data is available on the socket");
+    }
+
+	int read_buffer_size = 1024;
+	// TODO // Maybe rethink this high performance C like code for more modern safe code
+	// since we have recv and realloc which can both throw errors
+	char* read_buffer = (char*)calloc(read_buffer_size, sizeof(char));
+
+	int bytes_recived = 0;
+
+    recv(socket_peer, read_buffer, 1024, 0);
+
+	// do {
+		
+	// 	// Resize the read in buffer
+	// 	if (bytes_recived >= read_buffer_size){
+	// 		// Double input buffer size
+	// 		read_buffer = (char*)realloc(read_buffer, read_buffer_size *= 2);
+	// 	}
+	// // Keep reading while there is data
+	// } while ((bytes_recived = recv(socket_peer, read_buffer, 1024, 0)) > 0);
+
+	std::string msg = read_buffer;
+	free(read_buffer);
+
+
+	return msg;
+}
+
+int main()
+{
+    create("192.168.1.114", "27011");
+    connect();
+    while(true)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::string msg = recv();
+        if (msg.size() > 0)
+            std::cout << msg << std::endl;
+    }
+
+    return 0;
+}
---
+++ sendServer.cpp
@@ -0,0 +1,218 @@
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <Windows.h>
+
+// Threading headers
+#include <thread>
+#include <mutex>
+
+// other 
+#include <iostream>
+
+// #pragma comment(lib,"Ws2_32.lib")
+
+#define GETSOCKETERRNO() (WSAGetLastError())
+#define ISVALIDSOCKET(s) ((s) >= 0)
+
+// SOCKET socket_peer;
+// struct addrinfo* peer_address;
+
+WSADATA d;
+SOCKET socket_listen;
+SOCKET socket_client;
+
+bool create(std::string _port)
+{
+    // Set version for winsock2
+    #if defined(_WIN32)
+	// WSADATA d;
+	if (WSAStartup(MAKEWORD(2, 2), &d)) {
+        std::cerr << "Failed to initalize." << errno << std::endl;
+		return false;
+	}
+    #endif
+
+    // TODO ALTER TO C++
+	printf("Configuring local address...\n");
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	/* AF_INET = ipv4 */
+	hints.ai_family = AF_INET6;
+	/* TCP, soCK_DGRAM = UDP */
+	hints.ai_socktype = SOCK_STREAM;
+	/* Tells getaddrinfo() to bind to the wildcard address */
+	hints.ai_flags = AI_PASSIVE;
+
+	struct addrinfo* bind_address;
+	/* getaddrinfo here generates an address for bind() */
+	/* to do this we pass null as first param */
+	if (getaddrinfo(0, _port.c_str(), &hints, &bind_address)){
+		std::cerr << "mServerTCPCreate error.\ngetaddrinfo() failed. (" << GETSOCKETERRNO() << ")" << std::endl;
+		return false;
+	}
+
+	printf("Creating socket...\n");
+	// SOCKET socket_listen;
+	socket_listen = socket(bind_address->ai_family,
+		bind_address->ai_socktype, bind_address->ai_protocol);
+
+	/* check if the call to socket() was sucessful */
+	if (!ISVALIDSOCKET(socket_listen)) {
+        std::cerr << "socket() failed. (" << GETSOCKETERRNO() << ")" << std::endl;
+		return false;
+	}
+
+
+	/* implementation of duel stack ipv4 and ipv6 */
+	/* when an ipv4 connection connects the ipv4 is remapped to ipv6 */
+	/* first 96 bits are 0:0:0:0:0:ffff and the last 32 bits are the ipv4 address */
+
+	// WORKS
+// if (retCode = setsockopt(socket_listen, IPPROTO_IPV6, IPV6_V6ONLY, (char*)&retCode, sizeof(&retCode)))
+
+	// optCode has to be used otherwise we throw an error, dont know why. Just do it
+	int optCode = 0;
+	if (setsockopt(socket_listen, IPPROTO_IPV6, IPV6_V6ONLY, 
+				reinterpret_cast<char *>(&optCode), sizeof(optCode)))
+    {
+        std::cerr << "setsockopt() failed. (" << GETSOCKETERRNO() << ")" << std::endl;
+
+	}
+
+	/* Bindt socket to local address */
+	/* bind returns 0 on success*/
+	printf("Binding socket to local address...\n");
+	if (bind(socket_listen,
+			bind_address->ai_addr, bind_address->ai_addrlen)) {
+        std::cerr << "bind() failed. (" << GETSOCKETERRNO() << ")" << std::endl;
+		freeaddrinfo(bind_address); // In the case where bind fails the resorses are still freed
+		return false;
+	}
+	/* release address memory */
+	freeaddrinfo(bind_address);
+	return true;
+}
+
+bool listen(int connections)
+{
+    printf("listen() start");
+
+
+    /* Start listening */
+    std::cout << "Listening called..." << std::endl;
+	/* 10 connections are allowed to queue up */
+	if (listen(socket_listen, connections) < 0) {
+        std::cerr << "listen() failed. (" << GETSOCKETERRNO() << ")" << std::endl;
+		return false;
+	}
+    return true;
+
+    printf("listen() end");
+	/* accept any incoming connections */
+}
+
+bool accept()
+{
+    WSAPOLLFD fds[1];
+
+    fds[0].fd = socket_listen;
+    fds[0].events = POLLRDNORM;
+
+    // Sockets, number of sockets, timeout (ms)
+    int retCode = WSAPoll(fds, 1, 1);
+    if (retCode == SOCKET_ERROR)
+    {
+        printf("Error occured, errno: %d", errno);
+        return "";
+    }
+    else if (retCode == 0)
+    {
+        printf("Nothing yet.");
+        return "";
+    }
+    else
+    {
+        printf("Ready to accept");
+    }
+
+
+
+
+    std::cout << "acceptTCP() called" << std::endl;
+	/* we have to store the clients connection info */
+	/* the type will guarentee the type is large enough to hold this data */
+	struct sockaddr_storage client_address;
+	/* client len will differ depending on Ipv4/6*/
+	socklen_t client_len = sizeof(client_address);
+	/* now a tcp connection has been astablished */
+    /* potentially blocking*/
+	socket_client = accept(socket_listen,
+		(struct sockaddr*) &client_address, &client_len);
+
+	if (!ISVALIDSOCKET(socket_client)) {
+        std::cerr << "accept() failed. (" << GETSOCKETERRNO() << ")" << std::endl;
+		return false;
+	}
+
+	/* connection esablished, info */
+	printf("Client is connected...");
+	char address_buffer[100];
+
+	/* client address, client address length (for ipv4 or 6), output buffer, and length, hostname output(leave so null), "", Flag specifies we dont want to see hostname of ip addresses */
+	getnameinfo((struct sockaddr*)&client_address,
+		client_len, address_buffer, sizeof(address_buffer), 0, 0,
+		NI_NUMERICHOST);
+	printf("%s\n", address_buffer);
+
+    return true;
+}
+
+
+// Future method to send (DM to individual clients)
+bool send(std::string _msg)
+{
+	// Call serializer here
+
+    send(socket_client, _msg.c_str(), strlen(_msg.c_str()), 0);
+	return true;
+}
+
+int main()
+{
+    // fd_set readSet;
+    // for (int i = 0; i < 64; i++)
+    // {
+    //     SOCKET test = 0;
+    //     int number = 12;
+    //     readSet.fd_array[i] = test;
+    //     readSet.fd_count = number;
+    // }
+
+    // Use poll, then we time out and check the next value. Small timeouts like 5ms between each check.
+    // 
+    // We should do a "a full check of each client should take max 1 second". I.e. 4 clients, 250ms each. 100 clients, 10 ms
+
+
+    create("27011");
+    listen(1);
+
+    for(;;)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        accept();
+    }
+    
+    
+    while(true)
+    {
+        std::string message;
+        std::cout << "--> ";
+        std::getline(std::cin, message);
+        send(message);
+        std::cout << "Sent: " << message << "\n" << std::endl;
+    }
+
+
+    return 0;
+}
+

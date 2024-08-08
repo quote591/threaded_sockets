5ee2101937e4a4a2a264c85a57f0c3be4d81d314
Matt Bannister <matt.bann@live.co.uk>
2024-08-08 16:10:31 +0100

    Logging system implementation #1

--- src/logging.hpp
+++ src/logging.hpp
@@ -1,8 +1,18 @@
+#include <fstream>
+#include <chrono> // High precision clock
 
 class Log
 {
 private:
+    std::ofstream f;
+    std::chrono::high_resolution_clock::time_point startTimePoint = std::chrono::high_resolution_clock::now();
 
 public:
+    Log();
+    ~Log();
 
+    // @brief Logging function that writes message into file within logs/
+    // @args modelFunction - string of the method or system making the log
+    // @args message - log message
+    void m_LogWrite(std::string moduleFunction, std::string message);
 };
